88502c0b0ea276b4d2dee1c1eaae7be3905208c2
Feanor <hamzaaygan1995@gmail.com>
2022-08-06 14:06:11 +0300

    js session 1

---
+++ JavaScript/session 1/app.js
@@ -0,0 +1,56 @@
+console.log('app.js is running');
+
+// alert('cw')
+
+// let firstName = prompt('Enter your name :')
+// alert("First Name is" + ' ' + firstName);
+
+// let sonuc = confirm('Are you legal age?')
+// console.log(sonuc);
+
+var myNumber1 = 11;
+console.log(myNumber1)
+let v1 = 55;
+console.log(v1)
+/*
+
+some codes
+
+some functions
+
+
+
+
+*/
+
+var myNumber1 = 22;
+console.log(myNumber1)
+// let v1 = 88;  let ile aybı değişkene tekrar değer tanımlanmaz
+v1 = 88;  // ama var olan değer değiştirilebilir
+console.log(v1)
+
+
+
+// const mevzusu
+
+const student = {
+    name: "Feanor",
+    age: 27
+}
+console.log(student)
+
+const otherStudent = student;
+otherStudent.name = "Hamza"
+
+console.log(otherStudent)
+console.log(student)
+
+// primitive örnek
+
+let num1 = 581
+let num2 = 467
+
+num1 = num2
+num2 = num2 + 3
+
+console.log(num1, num2)
