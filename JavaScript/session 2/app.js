79be90a6d6a77a2a9fe79fd688c1dabf937c587a
Feanor <hamzaaygan1995@gmail.com>
2022-08-09 23:08:08 +0300

    s session 2

---
+++ JavaScript/session 2/app.js
@@ -0,0 +1,180 @@
+console.log('App js Running')
+
+/*var a = 5;
+console.log(a);
+
+{
+    var a = 3;
+    console.log(a);
+}
+
+console.log(a);*/
+
+/////////////////////////////////
+
+/*let a = 5;
+console.log(a);
+
+{
+    let a = 3;
+    console.log(a);
+}
+
+console.log(a);*/
+
+//////////////////////////////
+
+/*let a = 5;
+console.log(a);
+
+{
+    a = 3;
+    console.log(a);
+}
+
+console.log(a);*/
+
+/////////////////////////////////////
+
+/*console.log(typeof 0);
+console.log(typeof 3.14);
+console.log(typeof "hello");
+console.log(typeof (3>2));
+console.log(typeof true);
+console.log(typeof 8>10);*/
+
+////////////////////////////////////
+
+/*let b = +prompt("Bir sayı giriniz");
+console.log(b, typeof b);
+
+console.log(typeof typeof 42123);*/
+
+///////////////////////////////////
+
+/*let age;
+console.log(age); // undefined
+
+console.log(myAge);  // not defiend 
+
+console.log("hi");*/
+
+//////////////////////////////////
+
+/*let age = 30;
+let myAge = age
+age =31
+
+console.log(age);
+console.log(myAge);*/
+
+//////////////////////////////////
+
+/*let a = null;
+console.log(a, typeof a);
+
+null+3  // hata vermez
+undefined+3  // Nan verir*/
+
+///////////////////////////////////
+
+/*let x;
+
+do {
+    x = prompt("Give me a number : ");
+}while (isNaN(x));
+
+console.log(x, x*x);*/
+
+///////////////////////////////////
+
+/*console.log("12" == 12);
+console.log("12" === 12);
+console.log("12" * 12);*/
+
+///////////////////////////////////
+
+/*let x = 123456789001234
+let y = 123456789001234123
+
+console.log(x);
+console.log(y);*/
+
+//////////////////////////////////
+
+// console.log(0.1+0.2);
+
+///////////////////////////////////
+
+/*let g = 0.1+0.2
+console.log(g);
+console.log(+g.toFixed(2));*/
+
+///////////////////////////////////
+
+/*let h = 1_000_000_000
+console.log(h);*/
+
+///////////////////////////////////
+
+/*let firstName = `John`;
+let lastName = "Doe";
+let myCity = 'Tysons';
+console.log(`I'm ${firstName} ${lastName}. I live in ${myCity}`);
+console.log("I'm " + firstName + " " + lastName + ". I live in " + myCity);*/
+
+///////////////////////////////////
+
+/*let i = 'This is a string';
+let j = "This is a string";
+let k = `This is a string`;
+
+console.log(typeof i);
+console.log(typeof j);
+console.log(typeof k);*/
+
+//////////////////////////////////
+
+/*let userName = "Musa";
+console.log("Merhaba", userName);
+console.log(`Merhaba ${userName}`);
+console.log(`2 + 3 = ${2 + 3}`);
+console.log(`2 + 3 = ${"2" + "3"}`);
+console.log(userName.length);
+console.log(userName.concat("Feanor"));*/
+
+//////////////////////////////////
+
+/*let x = 8 > 5;
+let y = 42 == 35;
+
+console.log(`x = ${x}`);
+console.log(`x = ${y}`);*/
+
+///////////////////////////////////
+
+/*let m = ``;
+let n = ' ';
+let p = 0
+
+console.log(Boolean(m));
+console.log(Boolean(p));
+console.log(Boolean(n));*/
+
+////////////////////////////////////
+
+const myCar = {
+    make : 'Ford',
+    model : 'Mustang',
+    year : 1965,
+    color : 'Black'
+}
+console.log(myCar);
+
+myCar.color = 'Green';
+console.log(myCar);
+
+myCar.sunRoof = false;
+console.log(myCar);
+
+//////////////////////////////////////
